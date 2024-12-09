20428faadad8bbaef53496e0a4d93f2f0c3b5275
ng5134 <ng5134@bs.amalnet.k12.il>
2024-12-09 02:37:51 +0200

    second commit

--- app/src/main/java/com/example/ex1421b/MainActivity.java
+++ app/src/main/java/com/example/ex1421b/MainActivity.java
@@ -11,7 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
